9b56c4689a7b606d07c082686e961ccaa0074c89
Andy Waite <andyw8@users.noreply.github.com>
2018-12-18 08:36:12 -0500

    wip

--- lib/statement.rb
+++ lib/statement.rb
@@ -5,6 +5,23 @@ def statement(invoice, plays)
   render_plain_text(create_statement_data(invoice, plays))
 end
 
+def html_statement(invoice, plays)
+  render_html(create_statement_data(invoice, plays))
+end
+
+def render_plain_text(data)
+  result = "Statement for #{data["customer"]}\n"
+
+  data["performances"].each do |a_performance|
+    # print line for this order
+    result += "  #{a_performance["play"]["name"]}: #{usd(a_performance["amount"])} (#{a_performance["audience"]} seats)\n"
+  end
+
+  result += "Amount owed is #{usd(data["total_amount"])}\n"
+  result += "You earned #{data["total_volume_credits"]} credits\n"
+  result
+end
+
 def render_plain_text(data)
   result = "Statement for #{data["customer"]}\n"
 
--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -1,7 +1,7 @@
 require "json"
 require "statement"
 
-RSpec.describe "statement" do
+RSpec.describe "#statement" do
   specify do
     plays = JSON.parse(File.read("data/plays.json"))
     invoices = JSON.parse(File.read("data/invoices.json"))
@@ -16,6 +16,29 @@ RSpec.describe "statement" do
           Othello: $500.00 (40 seats)
         Amount owed is $1,730.00
         You earned 47 credits
+      STATEMENT
+    )
+  end
+end
+
+RSpec.describe "#html_statement" do
+  specify do
+    plays = JSON.parse(File.read("data/plays.json"))
+    invoices = JSON.parse(File.read("data/invoices.json"))
+
+    result = statement(invoices.first, plays)
+
+    expect(result).to eq(
+      <<~STATEMENT
+        <h1>Statement for BigCo</h1>
+        <table>
+          <tr><th>play</th><th>seats</th><th>cost</th></tr>
+          <tr><td>Hamlet</td><td>55</td><td>$650.00</td></tr>
+          <tr><td>As You Like It</td><td>35</td><td>$580.00</td></tr>
+          <tr><td>Othello</td><td>40</td><td>$500.00</td></tr>
+        </table>
+        <p>Amount owed is <em>$1,730.00</em>
+        <p>You earned <em>47</em> credits</p>
       STATEMENT
     )
   end
